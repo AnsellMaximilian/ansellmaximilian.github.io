3dee58b83723558958176348ebeba13d39a9a17b
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-07-13 12:31:26 +0700

    header

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,10 @@
-console.log('fag');
+const toggleBtn = document.getElementById("toggle-btn");
+const header = document.getElementById("header");
+const navList = document.getElementById("nav-list");
+toggleBtn.onclick = () => {
+    header.classList.toggle("open");
+};
+navList.onclick = () => {
+    header.classList.remove("open");
+};
 //# sourceMappingURL=main.js.map
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,11 @@
-console.log('fag')
+const toggleBtn = document.getElementById("toggle-btn");
+const header = document.getElementById("header");
+const navList = document.getElementById("nav-list");
+
+toggleBtn.onclick = () => {
+  header.classList.toggle("open");
+};
+
+navList.onclick = () => {
+  header.classList.remove("open");
+};
