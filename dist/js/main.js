a68bfe13073ed516510b87792a8607e45c3405b7
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-08-14 08:03:36 +0700

    project tabs

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,4 @@
+// NAVBAR MOBILE TOGGLE
 const toggleBtn = document.getElementById("toggle-btn");
 const header = document.getElementById("header");
 const navList = document.getElementById("nav-list");
@@ -7,4 +8,32 @@ toggleBtn.onclick = () => {
 navList.onclick = () => {
     header.classList.remove("open");
 };
+// PROJECT CARD TABS
+const projectCards = document.querySelectorAll(".project-list .card");
+projectCards.forEach((projectCard) => {
+    let tabs = projectCard.querySelectorAll(".tabs .tabs__item");
+    tabs.forEach((tab, index) => {
+        // Open first tab
+        if (index === 0) {
+            let targetItem = projectCard.querySelector(`.${tab.dataset.target}`);
+            tab.classList.add("open");
+            targetItem.classList.add("open");
+        }
+        tab.addEventListener("click", function () {
+            // Open the clicked tab
+            let targetItem = projectCard.querySelector(`.${tab.dataset.target}`);
+            tab.classList.add("open");
+            targetItem.classList.add("open");
+            // Close rest of tabs
+            tabs.forEach((otherTab) => {
+                if (otherTab !== tab) {
+                    let targetItem = projectCard.querySelector(`.${otherTab.dataset.target}`);
+                    otherTab.classList.remove("open");
+                    targetItem.classList.remove("open");
+                }
+            });
+        });
+    });
+    console.log(tabs);
+});
 //# sourceMappingURL=main.js.map
