4cccedebfe07ef03160445847ebcd09d234fc167
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-04-04 15:22:36 +0700

    Add hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Hero from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <main className="App">
+    <main className="App bg-black">
       <Navbar />
+      <Hero />
     </main>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <div className="container mx-auto text-white p-4 min-h-[16rem]">
+      <div>
+        <h1 className="text-center text-3xl font-semibold">
+          Ansell Maximilian
+        </h1>
+        <h2 className="text-xl text-center">Information Systems Graduate</h2>
+      </div>
+    </div>
+  );
+}
