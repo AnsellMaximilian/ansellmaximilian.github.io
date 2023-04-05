5fbe964bce0cb1715b360f4b9f5dc7c6e1e2894e
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-04-05 19:40:14 +0700

    Make hero responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,18 +3,22 @@ import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
-    <article className="text-white p-4 min-h-[16rem] mt-16">
-      <div className="relative">
-        <img src={hero} className="blur-sm brightness-50" />
+    <article className="text-white p-4 min-h-[16rem] h-[20rem] md:h-[24rem] mt-16">
+      <div className="relative h-full">
+        <img src={hero} className="blur-sm brightness-50 w-full" />
         <motion.div
           initial={{ y: 10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1 }}
-          className="absolute w-full h-full top-0 flex flex-col justify-center"
+          className="absolute w-full h-full top-0 flex flex-col justify-center -translate-y-1/2"
         >
           <div className="text-center max-w-xl mx-auto w-full">
-            <h1 className="text-3xl font-semibold">Ansell Maximilian</h1>
-            <h2 className="text-xl">Information Systems Graduate</h2>
+            <h1 className="textxl md:text-3xl font-semibold">
+              Ansell Maximilian
+            </h1>
+            <h2 className="text-base md:text-xl">
+              Information Systems Graduate
+            </h2>
           </div>
           <div
             className="mt-8 max-w-xl mx-auto p-4 rounded-lg w-full"
