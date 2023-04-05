167640f6a7f01a296c000f6da7354ee804eb1216
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-04-05 19:53:01 +0700

    Fix small sizes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,17 +3,17 @@ import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
-    <article className="text-white p-4 min-h-[16rem] h-[20rem] md:h-[24rem] mt-16">
+    <article className="text-white p-4 h-[24rem] md:h-[24rem] mt-16">
       <div className="relative h-full">
-        <img src={hero} className="blur-sm brightness-50 w-full" />
+        <img src={hero} className="blur-none md:blur-sm brightness-50 w-full" />
         <motion.div
           initial={{ y: 10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1 }}
-          className="absolute w-full h-full top-0 flex flex-col justify-center -translate-y-1/2"
+          className="absolute w-full h-full bottom-0 md:top-0 flex flex-col justify-end md:justify-center -translate-y-1/2"
         >
           <div className="text-center max-w-xl mx-auto w-full">
-            <h1 className="textxl md:text-3xl font-semibold">
+            <h1 className="text-2xl md:text-3xl font-semibold">
               Ansell Maximilian
             </h1>
             <h2 className="text-base md:text-xl">
@@ -21,14 +21,14 @@ export default function Hero() {
             </h2>
           </div>
           <div
-            className="mt-8 max-w-xl mx-auto p-4 rounded-lg w-full"
+            className="mt-2 md:mt-8 max-w-xl mx-auto p-4 rounded-lg w-full"
             id="about"
           >
             <h3 className="text-xl font-medium">About</h3>
-            <p>
+            <p className="text-justify indent-8 hyphens-auto">
               I am an Information Systems fresh graduate with concentration on
               Enterprise Applications based in Indonesia. A self-taught web
-              developer/prgrammer looking to get new opportunities and work to
+              developer/programmer looking to get new opportunities to work and
               learn.
             </p>
           </div>
