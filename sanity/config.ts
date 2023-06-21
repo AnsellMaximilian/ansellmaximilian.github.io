bb5805417d7a8f2af7ad85bf3b35c3af81c06424
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-06-21 12:25:25 +0700

    Fix configs

--- sanity/config.ts
+++ sanity/config.ts
@@ -1,14 +1,10 @@
 import { createClient, groq } from "next-sanity";
 import { cache } from "react";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION as string;
-
 export const config = {
-  projectId,
-  dataset,
-  apiVersion, // https://www.sanity.io/docs/api-versioning
+  projectId: "05gboql9",
+  dataset: "production",
+  apiVersion: "2023-05-03", // https://www.sanity.io/docs/api-versioning
   //   useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
 };
 
