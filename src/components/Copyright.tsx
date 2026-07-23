95707d516d91b2063ebf96a94377add429b344ea
Ansell Maximilian <56351143+AnsellMaximilian@users.noreply.github.com>
2026-07-23 12:54:09 +0700

    Rebuild/site 2026 (#1)
    * Replace portfolio with new Next.js site
    
    * Trigger Vercel preview
    
    * Trigger preview after Vercel repository migration
    
    * Trigger preview after upgrading node in vercel
    
    * Pin pnpm version for Vercel
    
    ---------
    
    Co-authored-by: Ansell Maximilian <ansell.maximilian@nexsoft.co.id>

--- src/components/Copyright.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Copyright = () => {
-  return (
-    <div className="text-gray-500 text-xs text-center">
-      © 2023 Ansell Maximilian. All Rights Reserved.
-    </div>
-  );
-};
